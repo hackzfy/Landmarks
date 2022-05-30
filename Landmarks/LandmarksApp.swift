de201b98bea4a89a0c1b228f188d315f44ca49d4
hackzfy <hackzfy@foxmail.com>
2022-05-31 01:34:45 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks2App.swift
+//  Landmarks2
+//
+//  Created by yang on 2022/5/30.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
